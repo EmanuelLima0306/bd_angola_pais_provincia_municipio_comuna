edd662d4a8b826b8d58c0da9cfa51b1de2d47342
Emanuel Lima <70851981+EmanuelLima0306@users.noreply.github.com>
2023-05-08 07:25:58 +0100

    Add files via upload

---
+++ bd_pais/provincia.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `facturabue` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `facturabue`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: facturabue
+-- ------------------------------------------------------
+-- Server version	5.7.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `provincia`
+--
+
+DROP TABLE IF EXISTS `provincia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `provincia` (
+  `id` bigint(20) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `designacao` varchar(255) NOT NULL,
+  `pais_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_jnxpgnppf1wqmy1dl9f0mm4mw` (`designacao`),
+  KEY `FKm4s599988w0v1q1nw6dyo5t2m` (`pais_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provincia`
+--
+
+LOCK TABLES `provincia` WRITE;
+/*!40000 ALTER TABLE `provincia` DISABLE KEYS */;
+INSERT INTO `provincia` VALUES (65,'2022-09-20 16:55:35','ACTIVADO','Bengo',64),(72,'2022-09-20 16:55:35','ACTIVADO','Benguela',64),(83,'2022-09-20 16:55:35','ACTIVADO','Bié',64),(93,'2022-09-20 16:55:35','ACTIVADO','Cabinda',64),(98,'2022-09-20 16:55:35','ACTIVADO','Cuando-Cubango',64),(108,'2022-09-20 16:55:35','ACTIVADO','Cuanza Norte',64),(119,'2022-09-20 16:55:35','ACTIVADO','Cuanza Sul',64),(132,'2022-09-20 16:55:35','ACTIVADO','Cunene',64),(139,'2022-09-20 16:55:35','ACTIVADO','Huambo',64),(151,'2022-09-20 16:55:35','ACTIVADO','Huíla',64),(166,'2022-09-20 16:55:35','ACTIVADO','Luanda',64),(176,'2022-09-20 16:55:35','ACTIVADO','Lunda Norte',64),(187,'2022-09-20 16:55:35','ACTIVADO','Lunda Sul',64),(192,'2022-09-20 16:55:35','ACTIVADO','Malanje',64),(207,'2022-09-20 16:55:35','ACTIVADO','Moxico',64),(217,'2022-09-20 16:55:35','ACTIVADO','Namibe',64),(223,'2022-09-20 16:55:35','ACTIVADO','Uíge',64),(240,'2022-09-20 16:55:35','ACTIVADO','Zaire',64);
+/*!40000 ALTER TABLE `provincia` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-04 18:24:04
